35b206a3ef7cf601655745fa416fe24267dd7471
cweipeng <39664929+cweipeng@users.noreply.github.com>
2018-05-27 14:04:59 +0800

    Add files via upload

---
+++ BMI.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num_a;
+	double w, h, BMI;
+	std::cout << "your weight(kg)\n";
+	cin >> w;
+	std::cout << "your height(m)\n";
+	cin >> h;
+	
+	
+		
+		BMI = w / (h*h);
+		
+		if (BMI < 18.5) cout << "underweight\n"; 
+		if (BMI >= 18.5&&BMI < 25) cout<< "normal\n";
+		if (BMI >= 25 && BMI < 30) cout <<"overweight";
+		if (BMI > 30) cout<<"obese";
+
+		cout << BMI<< endl;
+        return 0;
+}
+
